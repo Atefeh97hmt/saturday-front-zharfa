0255ddbf5da6ec9eb840721116dfc1d3eda2aaeb
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-03-15 18:17:27 +0330

    operators

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("this is my message");
-alert ("this is my message 2");
+// alert ("this is my message 2");
 console.error("this is error");
 console.warn("this is warn");
 
@@ -18,12 +18,23 @@ var isOnline = true;
 // var isOnline = false;
 console.log (isOnline);
 
-var a ="blue";
-var b ="red";
-var c = "green";
+// var a ="blue";
+// var b ="red";
+// var c = "green";
 
 
 var colors =[ "blue", "red", "green", "black" ];
 // console.log(colors);
 console.log(colors[3]);
 
+// operators
+var a = 3;
+var b = 2;
+console.log(a ** b);
+
+// ==
+// =!
+// <
+// >
+// =>
+// =<
